3cb2c98563898be7c27782b3f1bb5c28021ad433
shinhiy58@gmail.com <2601110281@gspace.kopo.ac.kr>
2026-06-11 11:46:19 +0900

    추상클래스, 인터페이스

---
+++ src/ai0611/abstracttest/RunPerson.java
@@ -0,0 +1,19 @@
+package ai0611.abstracttest;
+
+public class RunPerson {
+    public static void main(String[] args) {
+//        Person person = new Person();
+        Men men = new Men();
+        men.name = "김폴리";
+        men.weight = 70;
+        men.height = 180;
+
+        Women women = new Women();
+        women.name = "박인순";
+        women.weight = 48;
+        women.height = 170;
+
+
+
+    }
+}
